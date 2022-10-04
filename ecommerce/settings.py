3739b2de3816918c8ea821441e6f77d1c80027a6
yemreyolcu <yemrewho@outlook.com>
2022-10-05 01:24:25 +0300

    postgres added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': '4693',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
